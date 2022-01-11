ad05357b8541ecb4b4a8b09d3e76bdf40e07e3ec
Colby Wolfe <>
2022-01-11 16:23:34 -0500

    Add frequency detector

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@
 
 mod song;
 mod song_view;
+mod mic;
 
-use std::path::Path;
-use song::Song;
-use song_view::SongView;
-use cursive::views;
-use cursive::traits::Nameable;
+//use std::path::Path;
+//use song::Song;
+//use song_view::SongView;
+//use cursive::views;
+//use cursive::traits::Nameable;
 
 fn main() {
-    let song = Song::read(Path::new("test.song"));
+    mic::test();
+    //let song = Song::read(Path::new("test.song"));
 
-    let mut siv = cursive::default();
-    siv.add_global_callback('q', |s| s.quit());
-    siv.add_layer(views::ScrollView::new(SongView::new(song).with_name("view")).scroll_x(true));
-    siv.run();
+    //let mut siv = cursive::default();
+    //siv.add_global_callback('q', |s| s.quit());
+    //siv.add_layer(views::ScrollView::new(SongView::new(song).with_name("view")).scroll_x(true));
+    //siv.run();
 }
